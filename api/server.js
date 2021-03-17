ff9f3ad38c32710f29919a68568149ed1500cc01
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-17 16:17:00 -0400

    added a catch all to server to test API is alive

--- api/server.js
+++ api/server.js
@@ -7,4 +7,10 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+server.get('*', (req, res) => {
+    res.status(200).json({
+        message: 'The API Lives'
+    })
+})
+
 module.exports = server;
